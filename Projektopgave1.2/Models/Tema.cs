c4639062796f14ba44aa6438b8e94e5b2959240f
olliwh <98967791+olliwh@users.noreply.github.com>
2022-05-25 10:35:40 +0200

    added pop up to add tame. made tema name and kode required

--- Projektopgave1.2/Models/Tema.cs
+++ Projektopgave1.2/Models/Tema.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Projektopgave1._2.Models
 {
     public class Tema
     {
+        [Required(ErrorMessage = "Navn skal være udfyldt")]
         public string Name { get; set; }
         public int Id { get; set; }
         public string ImageName { get; set; }
         public string Description { get; set; }
+        [Required(ErrorMessage = "Kode skal være udfyldt")]
         public string Kode { get; set; }
     }
 }
