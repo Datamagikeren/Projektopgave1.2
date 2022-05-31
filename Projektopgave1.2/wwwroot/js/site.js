7615f445a756cc2a022e44cf36c3695f33df6b88
olliwh <98967791+olliwh@users.noreply.github.com>
2022-05-31 09:32:17 +0200

    tilfæjet dropdown hastighed til musicafspiller, flyttet JS og CSS til deres mapper

--- Projektopgave1.2/wwwroot/js/site.js
+++ Projektopgave1.2/wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+//play knap i musikafspiller
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+// Play song
+function playSong() {
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+}
+// Pause song
+function pauseSong() {
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+}
+// Event listeners
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+
+    if (isPlaying) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+});
